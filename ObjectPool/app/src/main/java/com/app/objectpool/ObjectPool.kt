3baec277c03ba9a7f2993340975b7b8fa01a62a1
AmitzurG <66904872+AmitzurG@users.noreply.github.com>
2020-09-14 11:38:25 +0300

    Update ObjectPool.kt

--- ObjectPool/app/src/main/java/com/app/objectpool/ObjectPool.kt
+++ ObjectPool/app/src/main/java/com/app/objectpool/ObjectPool.kt
@@ -1,14 +1,10 @@
 package com.app.objectpool
 
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.util.*
 
 class ObjectPool private constructor() {
     private val freeObjects: Queue<ReusedObject> = LinkedList()
-    private val usedObjects: Queue<ReusedObject> = LinkedList()
     private var objectNum = 1
 
     companion object {
@@ -21,21 +17,19 @@ class ObjectPool private constructor() {
     }
 
     // take object from the object pool
-    fun pull(): ReusedObject {
+    fun pull() : ReusedObject {
         if (freeObjects.isEmpty()) {
             Log.i(LOG_TAG, "there aren't unused objects, create another five objects")
             freeObjects.addAll(createObjects(5))
         }
         val obj = freeObjects.poll()
-        usedObjects.offer(obj)
         Log.i(LOG_TAG, "pull object ${obj.name}")
         return obj
     }
 
     // return object to reuse
-    private fun push(obj: ReusedObject) {
+    fun push(obj: ReusedObject) {
         Log.i(LOG_TAG, "push object ${obj.name}")
-        usedObjects.remove(obj)
         freeObjects.offer(obj)
     }
 
@@ -48,21 +42,12 @@ class ObjectPool private constructor() {
     }
 
     class ReusedObject(val name: String) {
-        var action: () -> Unit = {
-            // define the action that the reused object supposed to do, also can set any action
-        }
-
-        // possible to execute any action on the reused object
-        fun execute(action: () -> Unit = this.action) {
-            GlobalScope.launch {
-                // delay to demonstrate that the object in used
-                delay(2000)
-                action()
-                // after finish use the object return it to the pool
-                instant.push(this@ReusedObject)
-            }
+        @Throws(Throwable::class)
+        protected fun finalize() {
+            instant.push(this)
         }
     }
 }
+
 
 
